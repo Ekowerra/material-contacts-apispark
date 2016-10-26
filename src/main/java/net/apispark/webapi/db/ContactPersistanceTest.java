3f7875ad9b0cda7f804eb96a4888852283fea707
Adrien <adrien.ysnel@gmail.com>
2016-10-26 14:37:42 +0200

    Add file test
    File test add contact and ask id. Valid.

---
+++ src/main/java/net/apispark/webapi/db/ContactPersistanceTest.java
@@ -0,0 +1,23 @@
+package net.apispark.webapi.db;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import net.apispark.webapi.representation.Contact;
+
+public class ContactPersistanceTest {
+
+    @Test
+    public void add_contact() throws Exception {
+    	Contact contact = new Contact();
+        Assert.assertEquals(ContactPersistence.INSTANCE.addContact(contact), contact);
+    }
+    
+    @Test
+    public void ask_id() throws Exception {
+    	Contact contact = new Contact();
+    	contact.setId("a1");
+    	ContactPersistence.INSTANCE.addContact(contact);
+    	Assert.assertEquals(ContactPersistence.INSTANCE.getContact("a1"), contact);
+    }
+}
